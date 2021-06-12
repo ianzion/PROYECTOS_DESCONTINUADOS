31a64ddddcc44ce73377a3df8b4c5a8252a81939
ianzion <83738246+ianzion@users.noreply.github.com>
2021-06-12 03:17:38 -0300

    Update instalscript.sh

--- VPS-MX/Install/_Update/Original/instalscript.sh
+++ VPS-MX/Install/_Update/Original/instalscript.sh
@@ -63,12 +63,12 @@ else
 fi
 
 ubu16_fun () {
-    wget -O /etc/apt/sources.list https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/VPS-MX/main/VPS-MX_Oficial/Repositorios/16.04/sources.list &> /dev/null
+    wget -O /etc/apt/sources.list https://github.com/ianzion/PROYECTOS_DESCONTINUADOS/blob/master/VPS-MX/Install/_Update/VPS-MX_Oficial/Repositorios/16.04/sources.list &> /dev/null
 	echo -e "\033[1;97m SELECCIONO UBU 16"
 }
 
 ubu18_fun () {
-    wget -O /etc/apt/sources.list https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/VPS-MX/main/VPS-MX_Oficial/Repositorios/18.04/sources.list &> /dev/null
+    wget -O /etc/apt/sources.list https://github.com/ianzion/PROYECTOS_DESCONTINUADOS/blob/master/VPS-MX/Install/_Update/VPS-MX_Oficial/Repositorios/18.04/sources.list &> /dev/null
 	echo -e "\033[1;97m OK UBU 18"
 }	
 
